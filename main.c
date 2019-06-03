0d51cd8db316657caf2d7bb4affe791050a68173
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2019-06-03 17:32:02 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -7,130 +7,84 @@
 #include <wchar.h>
 
 
-#define MIN_PALABRAS 50000
+#define MIN_PALABRAS 5000
 
-wchar_t **leer_palabras(char *archivoEntrada, int *cant1, int *cant2, int size);
-int es_simbolo(char c);
+void leer_palabras(char *archivoEntrada);
+int es_simbolo(wchar_t c);
 //int es_espacio(char c);
-int es_fin_linea(char c);
+int es_fin_linea(wchar_t c);
 
 int main(int argc,  char **argv)
 {
 	setlocale(LC_CTYPE, "");
 
     char *archivoEntrada=malloc(sizeof(char) * 30), *archivoSalida=malloc(sizeof(char) * 30);
-    wchar_t **palabrasEntrada;
-    int size=MIN_PALABRAS, cantLineas=0, cantPalabras=0;
 
     strcpy(archivoEntrada, argv[1]);
     strcpy(archivoSalida, argv[2]);
-    printf("%s %s \n", archivoEntrada, archivoSalida);
-    printf("empiezo a leer_palabras:\n");
-    palabrasEntrada=leer_palabras(archivoEntrada, &cantLineas, &cantPalabras, size);
-    printf("termino de leer_palabras\n");
-    for(int i = 0; i < 6; i++){
-    	wprintf(L"%s\n", palabrasEntrada[i]);
-    }
+    wprintf(L"%s %s \n", archivoEntrada, archivoSalida);
+    wprintf(L"empiezo a leer_palabras:\n");
+    leer_palabras(archivoEntrada);
+    wprintf(L"termino de leer_palabras\n");
+
     free(archivoEntrada);
 
 }
 
-wchar_t** leer_palabras(char *archivoEntrada, int *cantLineas, int *cantPalabras, int size){
+void leer_palabras(char *archivoEntrada){
 	FILE* fp = fopen(archivoEntrada, "r");
 	assert(fp != NULL);
-	char auxC;
+	wchar_t auxC;
 	//printf("entro a la funcion\n");
-	wchar_t linea[100], **array=malloc(sizeof(wchar_t*) * MIN_PALABRAS);
-	int /*i, aux,*/ largoLinea, nroLineas=0, palabra=0;
+	wchar_t linea[100];
+	int ban=0, largoLinea, nroLineas=1;
 	
 	//”:”, ”;”, ”,”, ”.”, ”?”, ”!”.
 	while( (fwscanf(fp, L"%[^' '\r\n:;,.?!]", linea)) == 1 ){
-		//printf("entro al while\n");
-		//wprintf (L"%ls \n", L"A wide string"); //NO ME DA BOLA EN NADA
-		//wprintf (L"wtfff");
-		//wprintf (L"linea: %ls \n", linea);
-
 		// Se que el fwscanf se cruzo un caracter de terminacion y no lo leyo,
 		// asique lo leemos para descartarlo.
-		auxC = fgetc(fp);
+		auxC = fgetwc(fp);
+		wprintf(L"cant lineas: %ld \n", nroLineas);
 
 		if(es_fin_linea(auxC)){
+			ban=1;
+			wprintf(L"ahora viene la ultima palabra de la linea: %ld\n",ban);
 			// TODO Aca podes incrementar la cuenta de en que linea estas aldu.
 		}
 
 		// Luego leemos caracteres hasta cruzarnos con uno que no sea de terminacion de palabra.
 		while(es_simbolo(auxC)){
-			auxC = fgetc(fp);
+			auxC = fgetwc(fp);
 		}
 		// Hacemos un ungetc para que en la proxima leida del archivo, este caracter que no es de terminacion de palabra
 		// sea leido.
-		ungetc(auxC, fp);
+		ungetwc(auxC, fp);
 
-
-
-		if(palabra>=size){
-			//printf("no deberia entrar aca \n");
-			array=realloc(array, sizeof(wchar_t*) * (palabra+(size/2)));
-			size+=size/2;
-		}
-		
 		largoLinea = wcslen(linea);
-		printf("tamaño linea: %d\n",largoLinea );
 		linea[largoLinea] = '\0';		
-		array[nroLineas] = malloc(sizeof(wchar_t*) * (largoLinea + 1));
-		wcscpy(array[nroLineas], linea);
-		nroLineas++;
+		wprintf(L"tamaño linea: %ld\n",largoLinea );
+		wprintf(L"linea actual: %s \n", linea);
+		/*if(!esta_en_diccionario(linea)){
+			buscar_sugerencia(linea);
+			wprintf(L"linea %ld, "%s" no esta en el diccionario \n Quizas quiso decir: ", nroLineas, linea);
+		}*/
 
-		/*
-		for (i=0,aux=0; i < largoLinea; aux++, ++i)
-		{
-			if(es_simbolo(linea[aux])){
-				array[nroLineas][palabra] = malloc(sizeof(wchar_t*) * (aux+1));
-				memcpy(array[nroLineas][palabra],linea,aux);
-				array[nroLineas][palabra][aux]='\0';
-				i+=2; //por el espacio hasta el siguiente caracter
-				linea+=i;
-				aux=-1; //al finalizar le va a sumar 1
-				palabra++;
-				i--;
-			}
-
-			if(es_espacio(linea[aux])){
-				array[nroLineas][palabra] = malloc(sizeof(wchar_t*) * (aux+1));
-				memcpy(array[nroLineas][palabra],linea,aux);
-				array[nroLineas][palabra][aux]='\0';
-				i++;
-				linea+=i;
-				aux=-1;
-				palabra++;
-				i--;
-			}
-
+		if(ban==1){
+			nroLineas++;
+			wprintf(L"CANT lineas: %ld \n", nroLineas);
+			ban=0;
 		}
-		if(aux!=0){
-			array[nroLineas][palabra] = malloc(sizeof(wchar_t*) * (aux+1));
-			memcpy(array[nroLineas][palabra],linea,aux);
-			array[nroLineas][palabra][aux]='\0';
-			i++;
-			linea+=i;
-			aux=-1;
-			palabra++;
-		}
-	nroLineas++;
-	*/	
+
 	}
 	
-
-	*(cantLineas) = nroLineas;
 	fclose(fp);
-	return array;
 }
 
 //”:”, ”;”, ”,”, ”.”, ”?”, ”!”.
-int es_simbolo(char c){
+int es_simbolo(wchar_t c){
 	return ((c ==58) || (c==59) || (c==44) || (c==46) || (c==62) || (c==33) || (c==32) || (c=='\r') || (c=='\n') );
 }
 
-int es_fin_linea(char c){
+int es_fin_linea(wchar_t c){
 	return ( (c=='\r') || (c=='\n'));
 }
